af0cf578487941861b56c7711aaa6db5c85b790e
ben3amer <mohamedaminbnamer@gmail.com>
2022-08-24 21:16:04 +0100

    update_routers

--- src/routers/FormateurRouters.js
+++ src/routers/FormateurRouters.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const router = new express.Router()
 
+router.get('/formateur', (req, res) => {
+  res.send('formateur')
+})
+
 export default router
--- src/routers/SessionRouters.js
+++ src/routers/SessionRouters.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const router = new express.Router()
 
+router.get('/session', (req, res) => {
+  res.send('session')
+})
+
 export default router
