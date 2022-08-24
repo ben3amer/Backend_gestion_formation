39fb6c831b1df065331f30b24818694a204760ef
ben3amer <mohamedaminbnamer@gmail.com>
2022-08-24 21:46:46 +0100

    importin file

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ import userRouters from './src/routers/UserRouters.js'
 import formationRouters from './src/routers/FormateurRouters.js'
 import formateurRouters from './src/routers/FormationRouters.js'
 import sessionRouters from './src/routers/SessionRouters.js'
+import participantRouters from './src/routers/ParticipantRouters'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-
+app.use(participantRouters)
 app.use(userRouters)
 app.use(formateurRouters)
 app.use(formationRouters)
 app.use(sessionRouters)
-app.use(participantRouters)
+
 mongoose
   .connect(process.env.MONGODB_URL, () =>
     console.log('Connected successfully to database')
