28698a967e294d7365a3d1c2119de33d4329a360
ben3amer <mohamedaminbnamer@gmail.com>
2022-08-24 21:13:26 +0100

    Update_models

--- src/models/Formateur.js
+++ src/models/Formateur.js
@@ -1,7 +1,35 @@
 import mongoose from 'mongoose'
 
 const formateurSchema = new mongoose.Schema(
-  {},
+  {
+    Email  : {
+      type : String,
+      required : true,
+      unique : true,
+      trim : true,
+      lowercase : true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error("invalid email !");
+        }
+      },
+    },
+    Nom : {
+      type : String,
+      required : true,
+      trim :  true,
+    },
+    Prenom : {
+      type : String,
+      required : true,
+      trim : true, 
+    },
+    Tel : {
+      type : Number,
+      required : true,
+    },
+    
+  },
   {
     timestamps: true,
   }
--- src/models/Session.js
+++ src/models/Session.js
@@ -1,7 +1,37 @@
 import mongoose from 'mongoose'
 
 const sessionSchema = new mongoose.Schema(
-  {},
+  {
+    idFormation : {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Formation",
+    },
+    Date_debut : {
+      type : Date,
+      required : true,
+    },
+    Date_fin : {
+      type : Date,
+      required : true,
+    },
+    Description : {
+      type : String,
+      required : true,
+      trim : true,
+    },
+    Nb_participants : {
+      type : Number,
+      required : true,
+      default : 0,
+    },
+    
+    idFormateur : {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Formateur",
+    },
+  },
   {
     timestamps: true,
   }
